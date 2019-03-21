98636edebdbff8af0e8fc495daf619e84eb6c26d
Amarius1 <amarius2000@gmail.com>
2019-03-21 22:45:21 +0200

    Add files via upload

--- neolythe.js
+++ neolythe.js
@@ -327,3 +327,15 @@
     }, false);
 
 })(window);
+
+
+
+// Initialize
+const allCardsHeaders = document.getElementsByClassName("cardHeader");
+for(header of allCardsHeaders) {
+    header.addEventListener("click", clickCard);
+}
+
+function clickCard(e) {
+    this.parentElement.classList.toggle('cardActive');
+}
