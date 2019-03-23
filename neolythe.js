d9bcb42b67dd7509d46ff735ce9ddefe85a1f7d7
Amarius1 <amarius2000@gmail.com>
2019-03-23 18:02:09 +0200

    Add files via upload

--- neolythe.js
+++ neolythe.js
@@ -330,12 +330,17 @@
 
 
 
-// Initialize
-const allCardsHeaders = document.getElementsByClassName("cardHeader");
-for(header of allCardsHeaders) {
-    header.addEventListener("click", clickCard);
-}
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-function clickCard(e) {
-    this.parentElement.classList.toggle('cardActive');
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
